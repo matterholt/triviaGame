d312113ada0f4a7ddffb5abb1421dfd875a1ea75
MAtterholt <matterholt84@gmail.com>
2020-09-28 14:20:54 -0400

    contect for question to answer

--- trivia_app/src/App.js
+++ trivia_app/src/App.js
@@ -3,21 +3,29 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
+import QuizResults from "./pages/QuizResults";
+
+import { QuestionProvider } from "./context/questionContext";
 
 // create routing file for all routes, (might be easier to manage??)
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/Quiz">
-          <Quiz />
-        </Route>
-      </Switch>
-    </Router>
+    <QuestionProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/Quiz">
+            <Quiz />
+          </Route>
+          <Route exact path="/QuizResults">
+            <QuizResults />
+          </Route>
+        </Switch>
+      </Router>
+    </QuestionProvider>
   );
 }
 
---
+++ trivia_app/src/context/questionContext.js
@@ -0,0 +1,113 @@
+import React from "react";
+const test_questions = {
+  response_code: 0,
+  results: [
+    {
+      category: "Vehicles",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "In 1993 Swedish car manufacturer Saab experimented with replacing the steering wheel with a joystick in a Saab 9000.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "hard",
+      question: "DHCP stands for Dynamic Host Configuration Port.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Entertainment: Film",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "&quot;Cube&quot;, &quot;Cube 2: Hypercube&quot; and &quot;Cube Zero&quot; were directed by the same person.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Politics",
+      type: "boolean",
+      difficulty: "hard",
+      question: "Joko Widodo has appeared in the cover of a TIME magazine.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "Entertainment: Music",
+      type: "boolean",
+      difficulty: "hard",
+      question: "The band STRFKR was also briefly known as Pyramiddd.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "hard",
+      question: "The USS Missouri (BB-63) last served in the Korean War.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Entertainment: Japanese Anime & Manga",
+      type: "boolean",
+      difficulty: "hard",
+      question: "Druid is a mage class in &quot;Log Horizon&quot;.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Politics",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "George Clinton, Vice President of the United States (1805-1812), is an ancestor of President Bill Clinton.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Entertainment: Japanese Anime & Manga",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "In the &quot;Kagerou Daze&quot; series, Shintaro Kisaragi is prominently shown with the color red.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "Art",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "The Statue of Liberty&#039;s official name is &ldquo;Liberty Enlightening the World&rdquo;.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+  ],
+};
+
+const TriviaQuestions = React.createContext();
+
+function QuestionProvider({ children }) {
+  return (
+    <TriviaQuestions.Provider value={test_questions.results}>
+      {children}
+    </TriviaQuestions.Provider>
+  );
+}
+
+function useCountState() {
+  // keep the provider in the context folder allowing central location
+  const context = React.useContext(TriviaQuestions);
+  if (context === undefined) {
+    throw new Error("useCountState must be used within a CountProvider");
+  }
+
+  return context;
+}
+
+export { QuestionProvider, useCountState };
