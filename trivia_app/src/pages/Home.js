735cf3a665a4c9a2664f75b81209a7cd8bb922a1
MAtterholt <matterholt84@gmail.com>
2020-09-27 22:59:17 -0400

    simple home page

---
+++ trivia_app/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Trivia Challenge</h1>
+      <p>You will be precented with 10 True or False questions</p>
+      <p>Can you score 100%</p>
+      <Link to="/Quiz">BEGIN</Link>
+    </div>
+  );
+};
+
+export default Home;
