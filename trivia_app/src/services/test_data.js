80ecee01e1c24222199c41a6e783e01d8b0900ff
MAtterholt <matterholt84@gmail.com>
2020-09-28 22:29:18 -0400

    api sampe data, remove svg

---
+++ trivia_app/src/services/test_data.js
@@ -0,0 +1,90 @@
+export const test_questions = {
+  response_code: 0,
+  results: [
+    {
+      category: "Vehicles",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "In 1993 Swedish car manufacturer Saab experimented with replacing the steering wheel with a joystick in a Saab 9000.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "hard",
+      question: "DHCP stands for Dynamic Host Configuration Port.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Entertainment: Film",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "&quot;Cube&quot;, &quot;Cube 2: Hypercube&quot; and &quot;Cube Zero&quot; were directed by the same person.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Politics",
+      type: "boolean",
+      difficulty: "hard",
+      question: "Joko Widodo has appeared in the cover of a TIME magazine.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "Entertainment: Music",
+      type: "boolean",
+      difficulty: "hard",
+      question: "The band STRFKR was also briefly known as Pyramiddd.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "History",
+      type: "boolean",
+      difficulty: "hard",
+      question: "The USS Missouri (BB-63) last served in the Korean War.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Entertainment: Japanese Anime & Manga",
+      type: "boolean",
+      difficulty: "hard",
+      question: "Druid is a mage class in &quot;Log Horizon&quot;.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Politics",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "George Clinton, Vice President of the United States (1805-1812), is an ancestor of President Bill Clinton.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Entertainment: Japanese Anime & Manga",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "In the &quot;Kagerou Daze&quot; series, Shintaro Kisaragi is prominently shown with the color red.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+    {
+      category: "Art",
+      type: "boolean",
+      difficulty: "hard",
+      question:
+        "The Statue of Liberty&#039;s official name is &ldquo;Liberty Enlightening the World&rdquo;.",
+      correct_answer: "True",
+      incorrect_answers: ["False"],
+    },
+  ],
+};
